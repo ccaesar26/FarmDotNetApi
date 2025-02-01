4ee945a9ba6876a6a705b4d222320202befdb271
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2025-02-01 18:58:06 +0200

    Create User.cs

---
+++ IdentityService/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IdentityService.Models;
+
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; init; }
+    
+    [Required]
+    [MaxLength(120)]
+    public required string Username { get; init; }
+
+    [Required]
+    [MaxLength(120)]
+    [EmailAddress]
+    public required string Email { get; init; }
+    
+    [Required]
+    [MaxLength(120)]
+    public required string PasswordHash { get; init; }
+    
+    [Required]
+    [MaxLength(120)]
+    public required string Role { get; init; }
+    
+    public Guid? FarmId { get; set; }
+}
