a12c63b9ebaa5d0442972c0ee84d24ad7bd0846d
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2025-04-05 13:25:58 +0300

    Still no idea

---
+++ FarmerTasksService/Migrations/20250403103714_AddedCropId.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FarmerTasksService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCropId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FieldId",
+                table: "Tasks",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CropId",
+                table: "Tasks",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CropId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FieldId",
+                table: "Tasks",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+        }
+    }
+}
